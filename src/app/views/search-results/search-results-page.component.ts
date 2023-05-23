b3910e225c55b5373a89fc71a5a79829e84218aa
Ruben <rgarcia@odec.es>
2023-05-23 10:59:56 +0200

    fix: search view

--- src/app/views/search-results/search-results-page.component.ts
+++ src/app/views/search-results/search-results-page.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { SearchResultsService } from './search-results.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results-page.component.html',
   styleUrls: ['./search-results-page.component.css']
 })
-export class SearchResultsPageComponent implements OnInit {
+export class SearchResultsPageComponent implements OnInit, OnDestroy {
+
+  private routeParamsSubscription!: Subscription;
 
   constructor(
     private route: ActivatedRoute,
@@ -15,12 +18,19 @@ export class SearchResultsPageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.searchCars();
+    this.routeParamsSubscription = this.route.params.subscribe((params: Params) => {
+      this.searchCars(params['query']);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeParamsSubscription) {
+      this.routeParamsSubscription.unsubscribe();
+    }
   }
   
 
-  searchCars() {
-    const query = this.route.snapshot.paramMap.get('query');
+  searchCars(query: string) {
     if (query) {
       this.searchResultsService.getCars(query).subscribe(res => {
         console.log(res);
