5345aee89f3bdf25e0b2787d5d649ccefc9d72f0
Ruben <rgarcia@odec.es>
2023-06-08 10:09:28 +0200

    fix: dont upload car without login

--- src/app/views/custom-cars/upload-custom-car-page/upload-custom-car-page.component.ts
+++ src/app/views/custom-cars/upload-custom-car-page/upload-custom-car-page.component.ts
@@ -44,97 +44,104 @@ export class UploadCustomCarPageComponent implements OnInit {
   }
 
   async submitForm() {
-    // restart notifications
-    this.formError = false;
-    this.formSuccess = false;
-
-    // disable submit button
-    this.submitButtonRef.nativeElement.disabled = true;
-    
-    // data of form
-    const car_id = this.customCarForm.get('car_id')?.value;
-    const model_name = this.customCarForm.get('model_name')?.value;
-    const year = this.customCarForm.get('year')?.value;
-    const brand = this.customCarForm.get('brand')?.value;
-
-    // validations
-    if (car_id.length < 3) {
-      this.errorMsg = 'POST_CUSTOM_CAR_BAD_ID';
-      this.formError = true;
+    if (this.userToken.hasToken) {
+      // restart notifications
+      this.formError = false;
       this.formSuccess = false;
-    
-      // activate submit button
-      this.submitButtonRef.nativeElement.disabled = false;
-      return;
-    }
-
-    if (model_name.length < 3) {
-      this.errorMsg = 'POST_CUSTOM_CAR_BAD_MODEL';
-      this.formError = true;
-      this.formSuccess = false;
-    
-      // activate submit button
-      this.submitButtonRef.nativeElement.disabled = false;
-      return;
-    }
-
-    if (year > 2025 || year < 1950) {
-      this.errorMsg = 'POST_CUSTOM_CAR_BAD_YEAR';
-      this.formError = true;
-      this.formSuccess = false;
-    
-      // activate submit button
-      this.submitButtonRef.nativeElement.disabled = false;
-      return;
-    }
-
-    if (brand.length < 3) {
-      this.errorMsg = 'POST_CUSTOM_CAR_BAD_BRAND';
-      this.formError = true;
-      this.formSuccess = false;
-    
-      // activate submit button
-      this.submitButtonRef.nativeElement.disabled = false;
-      return;
-    }
-
-    this.formError = false;
-
-    const bodyRequest = {
-      car_id: car_id,
-      model_name: model_name,
-      year: year,
-      brand: brand,
-    }
-
-    try {
-      if (this.userToken.hasToken && this.userToken.userId) {
-        const res = await this.customCarsService.uploadCustomCar(this.userToken.userId, bodyRequest).toPromise();
-        // save id of custom car for upload imgs
-        this.customCarId = res.customCar.id;
-
-        this.successMsg = 'CUSTOM_CAR_UPLOADED';
-        this.formSuccess = true;
   
-        // activate submit button
-        this.submitButtonRef.nativeElement.disabled = true;
-
-        // end form
-        this.customCarUploaded = true;
-      }
+      // disable submit button
+      this.submitButtonRef.nativeElement.disabled = true;
       
-    } catch (err: any) {
-      console.error(err);
-      this.submitButtonRef.nativeElement.disabled = false;
-
-      if (err.error.errors[0].param === 'brand') {
-        this.errorMsg = 'POST_CUSTOM_CAR_BAD_BRAND';
+      // data of form
+      const car_id = this.customCarForm.get('car_id')?.value;
+      const model_name = this.customCarForm.get('model_name')?.value;
+      const year = this.customCarForm.get('year')?.value;
+      const brand = this.customCarForm.get('brand')?.value;
+  
+      // validations
+      if (car_id.length < 3) {
+        this.errorMsg = 'POST_CUSTOM_CAR_BAD_ID';
         this.formError = true;
+        this.formSuccess = false;
+      
+        // activate submit button
+        this.submitButtonRef.nativeElement.disabled = false;
         return;
       }
   
-      this.errorMsg = 'UNEXPECTED_ERROR';
+      if (model_name.length < 3) {
+        this.errorMsg = 'POST_CUSTOM_CAR_BAD_MODEL';
+        this.formError = true;
+        this.formSuccess = false;
+      
+        // activate submit button
+        this.submitButtonRef.nativeElement.disabled = false;
+        return;
+      }
+  
+      if (year > 2025 || year < 1950) {
+        this.errorMsg = 'POST_CUSTOM_CAR_BAD_YEAR';
+        this.formError = true;
+        this.formSuccess = false;
+      
+        // activate submit button
+        this.submitButtonRef.nativeElement.disabled = false;
+        return;
+      }
+  
+      if (brand.length < 3) {
+        this.errorMsg = 'POST_CUSTOM_CAR_BAD_BRAND';
+        this.formError = true;
+        this.formSuccess = false;
+      
+        // activate submit button
+        this.submitButtonRef.nativeElement.disabled = false;
+        return;
+      }
+  
+      this.formError = false;
+  
+      const bodyRequest = {
+        car_id: car_id,
+        model_name: model_name,
+        year: year,
+        brand: brand,
+      }
+  
+      try {
+        if (this.userToken.hasToken && this.userToken.userId) {
+          const res = await this.customCarsService.uploadCustomCar(this.userToken.userId, bodyRequest).toPromise();
+          // save id of custom car for upload imgs
+          this.customCarId = res.customCar.id;
+  
+          this.successMsg = 'CUSTOM_CAR_UPLOADED';
+          this.formSuccess = true;
+    
+          // activate submit button
+          this.submitButtonRef.nativeElement.disabled = true;
+  
+          // end form
+          this.customCarUploaded = true;
+        }
+        
+      } catch (err: any) {
+        console.error(err);
+        this.submitButtonRef.nativeElement.disabled = false;
+  
+        if (err.error.errors[0].param === 'brand') {
+          this.errorMsg = 'POST_CUSTOM_CAR_BAD_BRAND';
+          this.formError = true;
+          return;
+        }
+    
+        this.errorMsg = 'UNEXPECTED_ERROR';
+        this.formError = true;
+      }
+
+    } else {
+      this.errorMsg = 'GN_TOKEN_EXPIRED';
       this.formError = true;
+
     }
   }
 
