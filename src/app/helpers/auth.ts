efacd9aca48d33920f01ad8968e3db75bb6c3c76
Ruben G <rgarcia@odec.es>
2023-08-01 14:46:37 +0200

    User creation validate

--- src/app/helpers/auth.ts
+++ src/app/helpers/auth.ts
@@ -1,19 +1,19 @@
 export const isValidEmail = (email: string) => {
-    const correoPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const correoPattern = /^[a-zA-Z0-9._%+-]{1,18}@[a-zA-Z0-9.-]{1,10}\.[a-zA-Z]{2,4}$/;
 
     return correoPattern.test(email);
 }
 
 // Validation: Valid password format
 export const isValidPassword = (password: string) => {
-    const passwordPattern = new RegExp('^[a-zA-Z0-9_,-.*!?¿çñ@]{8,}$');
+    const passwordPattern = new RegExp('^[a-zA-Z0-9_,-.*!?¿çñ@]{8,16}$');
     
     return passwordPattern.test(password);
 }
 
 // Validation: Valid username format
 export const isValidUsername = (username: string) => {
-    const usernamePattern = new RegExp('^[a-zA-Z0-9_ñç]{3,}$');
+    const usernamePattern = new RegExp('^[a-zA-Z0-9_ñç]{3,10}$');
 
     return usernamePattern.test(username);
 }
