0203427fa0fe06b3fda278ec5233636af7947fd2
Ruben G <rgarcia@odec.es>
2023-08-10 21:15:22 +0200

    feat: 2023 basic cars added

--- src/app/views/basic-cars-page/basic-cars-page.component.ts
+++ src/app/views/basic-cars-page/basic-cars-page.component.ts
@@ -19,10 +19,10 @@ export class BasicCarsPageComponent implements OnInit {
   cars: any[] = [];
   showedCars: any[] = [];
 
-  selectedYear: string = '2022';
+  selectedYear: string = '2023';
   selectedSerie: string = 'All';
 
-  availableYears = ['2022', '2021', '2020', '2019', '2018'];
+  availableYears = ['2023', '2022', '2021', '2020', '2019', '2018'];
   availableSeries = [];
 
   error = false;
