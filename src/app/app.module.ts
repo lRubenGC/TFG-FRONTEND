fdd2dff20ca4617cbdc4fb86ec2eca85e6446563
Ruben G <rgarcia@odec.es>
2023-11-04 18:30:13 +0100

    GA4: tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
 // Angular libs
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // Base App
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// GA4
+import { GtagModule } from 'angular-gtag';
 
 // Module Views
+import { ComponentsModule } from './components/components.module';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthModule } from './views/auth/auth.module';
 import { BasicCarsPageModule } from './views/basic-cars-page/basic-cars-page.module';
 import { CustomCarsModule } from './views/custom-cars/custom-cars.module';
+import { DetailedCarPageModule } from './views/detailed-car-page/detailed-car-page.module';
+import { InfoModule } from './views/info/info.module';
 import { LandingPageModule } from './views/landing-page/landing-page.module';
 import { PremiumCarsPageModule } from './views/premium-cars-page/premium-cars-page.module';
-import { UserModule } from './views/user/user.module';
-import { AuthModule } from './views/auth/auth.module';
 import { SearchResultsPageModule } from './views/search-results/search-results-page.module';
-import { AuthGuard } from './guards/auth.guard';
-import { ComponentsModule } from './components/components.module';
-import { InfoModule } from './views/info/info.module';
-import { DetailedCarPageModule } from './views/detailed-car-page/detailed-car-page.module';
-
+import { UserModule } from './views/user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -39,9 +39,12 @@ import { DetailedCarPageModule } from './views/detailed-car-page/detailed-car-pa
         useFactory: (http: HttpClient) => {
           return new TranslateHttpLoader(http, './assets/i18n/', '.json');
         },
-        deps: [HttpClient]
-      }
+        deps: [HttpClient],
+      },
     }),
+
+    // GA4
+    GtagModule.forRoot({ trackingId: 'G-P9GYT9HZTD', trackPageviews: true }),
 
     // App Modules
     AuthModule,
@@ -53,11 +56,9 @@ import { DetailedCarPageModule } from './views/detailed-car-page/detailed-car-pa
     LandingPageModule,
     PremiumCarsPageModule,
     SearchResultsPageModule,
-    UserModule
+    UserModule,
   ],
-  providers: [
-    AuthGuard
-  ],
-  bootstrap: [AppComponent]
+  providers: [AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -3,19 +3,19 @@ import { TranslateService } from '@ngx-translate/core';
 
 import { lastValueFrom, Subscription } from 'rxjs';
 
-import { LanguageService } from "../../services/language.service";
+import { Gtag } from 'angular-gtag';
+import { decodeToken } from 'src/app/helpers/generics';
 import { landingCardInterface } from 'src/app/models/landingPage.interface';
 import { msgCardInterface } from 'src/app/models/shared.interface';
-import { decodeToken } from 'src/app/helpers/generics';
 import { LoaderService } from 'src/app/services/loader.service';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit, OnDestroy {
-
   userLoggedIn!: boolean;
 
   landingCards: landingCardInterface[] = [
@@ -23,29 +23,29 @@ export class LandingPageComponent implements OnInit, OnDestroy {
       backgroundImg: '../../../assets/images/landing_page/basic_bg.webp',
       popImg: '../../../assets/images/landing_page/basic_pop.webp',
       linkTo: 'basic-cars',
-      title: 'SEARCH_BASIC_CARS'
+      title: 'SEARCH_BASIC_CARS',
     },
     {
       backgroundImg: '../../../assets/images/landing_page/premium_bg.webp',
       popImg: '../../../assets/images/landing_page/premium_pop.webp',
       linkTo: 'premium-cars',
-      title: 'SEARCH_PREMIUM_CARS'
+      title: 'SEARCH_PREMIUM_CARS',
     },
     {
       backgroundImg: '../../../assets/images/landing_page/custom_bg.webp',
       popImg: '../../../assets/images/landing_page/custom_pop.webp',
       linkTo: 'custom-cars',
-      title: 'CUSTOM_CARS'
-    }
-  ]
+      title: 'CUSTOM_CARS',
+    },
+  ];
 
   msg_card: msgCardInterface = {
     title: '',
     description: [],
     button: false,
     buttonName: '',
-    buttonLink: '/auth'
-  }
+    buttonLink: '/auth',
+  };
 
   private subscription!: Subscription;
 
@@ -53,16 +53,19 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     private translate: TranslateService,
     private languageService: LanguageService,
     private loaderService: LoaderService,
+    private gtag: Gtag
   ) {}
 
   async ngOnInit() {
     const token = await decodeToken();
     this.msg_card.button = !token.hasToken;
 
-    this.subscription = this.languageService.languageChanged$.subscribe(lang => {
-      this.translate.use(lang);
-      this.changeLanguage();
-    })
+    this.subscription = this.languageService.languageChanged$.subscribe(
+      (lang) => {
+        this.translate.use(lang);
+        this.changeLanguage();
+      }
+    );
 
     this.changeLanguage();
   }
@@ -91,9 +94,14 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     this.msg_card.buttonName = await lastValueFrom(cardButton);
   }
 
+  trackEvent(view: string) {
+    this.gtag.event('click', {
+      event_category: 'button',
+      event_label: view,
+    });
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-
 }
-
