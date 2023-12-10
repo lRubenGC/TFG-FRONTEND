6636e4dafee7c11502bb850503f71e778ceed825
lRubenGC <lRubenGC@gmail.com>
2023-12-10 17:30:06 +0100

    useless components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,34 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { TranslateModule } from '@ngx-translate/core';
 
-import { CustomCardComponent } from './car-cards/custom-card/custom-card.component';
-import { PremiumPillComponent } from './premium-pill/premium-pill.component';
-import { ButtonsLegendComponent } from './shared/buttons-legend/buttons-legend.component';
-import { DCButtonComponent } from './shared/dc-button/dc-button.component';
-import { DCSecondaryButtonComponent } from './shared/dc-secondary-button/dc-secondary-button.component';
-import { FilterPillComponent } from './shared/filter-pill/filter-pill.component';
-import { GlobalNotificationComponent } from './shared/global-notification/global-notification.component';
-import { MsgCardComponent } from './shared/msg-card/msg-card.component';
-import { ScrollToTopComponent } from './shared/scroll-to-top/scroll-to-top.component';
-import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { UploadImgComponent } from './shared/upload-img/upload-img.component';
 import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
-  declarations: [
-    ButtonsLegendComponent,
-    CustomCardComponent,
-    DCButtonComponent,
-    DCSecondaryButtonComponent,
-    FilterPillComponent,
-    MsgCardComponent,
-    PremiumPillComponent,
-    GlobalNotificationComponent,
-    ScrollToTopComponent,
-    SpinnerComponent,
-    UserCardComponent,
-    UploadImgComponent,
-  ],
+  declarations: [UserCardComponent, UploadImgComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -40,19 +17,6 @@ import { UserCardComponent } from './user-card/user-card.component';
     ReactiveFormsModule,
     TranslateModule,
   ],
-  exports: [
-    ButtonsLegendComponent,
-    CustomCardComponent,
-    DCButtonComponent,
-    DCSecondaryButtonComponent,
-    FilterPillComponent,
-    MsgCardComponent,
-    PremiumPillComponent,
-    GlobalNotificationComponent,
-    ScrollToTopComponent,
-    SpinnerComponent,
-    UserCardComponent,
-    UploadImgComponent,
-  ],
+  exports: [UserCardComponent, UploadImgComponent],
 })
 export class ComponentsModule {}
