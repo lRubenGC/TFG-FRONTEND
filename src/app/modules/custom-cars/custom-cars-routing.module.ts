5c9f6a4893e3bf338a60dbb14ecfa544c5cb2a42
lRubenGC <lRubenGC@gmail.com>
2023-12-20 20:20:30 +0100

    feat: upload guard

--- src/app/modules/custom-cars/custom-cars-routing.module.ts
+++ src/app/modules/custom-cars/custom-cars-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 import { CustomCarsEditView } from './views/custom-cars-edit/custom-cars-edit.component';
 import { CustomCarsListView } from './views/custom-cars-list/custom-cars-list.component';
 import { CustomCarsUploadView } from './views/custom-cars-upload/custom-cars-upload.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'upload',
     component: CustomCarsUploadView,
+    canActivate: [AuthGuard],
   },
   {
     path: 'edit',
--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,24 +1,51 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { Observable, map } from 'rxjs';
+import { CanActivate } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { MessageService } from 'primeng/api';
+import { Observable, lastValueFrom, map } from 'rxjs';
 import { AuthService } from '../../modules/auth/services/auth.service';
+import { ITOAST_OBJECT } from '../models/toast-shared.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private messageService: MessageService,
+    private translate: TranslateService
+  ) {}
 
   canActivate(): Observable<boolean> | boolean {
     return this.authService.isUserLoggedIn$.pipe(
       map((isLoggedIn) => {
         if (!isLoggedIn) {
-          this.router.navigate(['/auth']);
+          this.showToast({
+            severity: 'error',
+            summary: 'toast.error',
+            detail: 'toast.not_logged_in',
+          });
           return false;
         }
 
         return true;
       })
     );
+  }
+
+  public async showToast(toastObject: ITOAST_OBJECT) {
+    const summaryT = this.translate.get(toastObject.summary);
+    const summary = await lastValueFrom(summaryT);
+
+    const detailT = this.translate.get(toastObject.detail);
+    const detail = await lastValueFrom(detailT);
+
+    this.messageService.add({
+      key: 'br',
+      severity: toastObject.severity,
+      summary: summary,
+      detail: detail,
+      life: 2000,
+    });
   }
 }
