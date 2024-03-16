15ec1e135593f94255d52d536ead44906b0e6611
lRubenGC <lRubenGC@gmail.com>
2024-03-16 20:13:56 +0100

    feat: privacy policies update

--- src/app/modules/google/privacy-policies/view/privacy-policies.component.ts
+++ src/app/modules/google/privacy-policies/view/privacy-policies.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'privacy-policies',
   templateUrl: './privacy-policies.component.html',
   styleUrls: ['./privacy-policies.component.scss'],
 })
-export class PrivacyPoliciesComponent {}
+export class PrivacyPoliciesComponent {
+  @ViewChildren('firstWordStrong') firstWordStrong!: QueryList<ElementRef>;
+  ngAfterViewInit() {
+    this.firstWordStrong.forEach((element: ElementRef) => {
+      const words = element.nativeElement.textContent.split(' ');
+      words[0] = '<span style="font-weight: 600;">' + words[0] + '</span>';
+      element.nativeElement.innerHTML = words.join(' ');
+    });
+  }
+}
